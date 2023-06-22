634aeaad1769ba5e0a1621cfe3bb1e47ba1941fa
Fabricio Matos <43764604+Fabritz@users.noreply.github.com>
2023-06-22 17:46:04 -0600

    cambiamos a Prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,17 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { UmlModule } from './uml/uml.module';
-import { config } from './orm.config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { GruposModule } from './grupos/grupos.module';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(config),
-    AuthModule,
-    UsersModule,
-    UmlModule,
-    GruposModule,
-  ],
+  imports: [AuthModule, UsersModule, UmlModule, GruposModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  signup() {
+    return 'ok';
+  }
+
+  signin() {
+    return 'si';
+  }
+}
